7a4edb904ce8f71d9f082e7e3785ae3caa24df08
hrchaithra <46788513+hrchaithra@users.noreply.github.com>
2023-08-22 16:48:15 +0530

    Create s3-lamdba-function.py

---
+++ AWS Event Triggering/s3-lambda-function/s3-lamdba-function.py
@@ -0,0 +1,27 @@
+import boto3
+import json
+
+def lambda_handler(event,context):
+  # Getting information from the s3 even trigger
+  bucket_name = event['Records'][0]['s3']['bucket']['name']
+  object_key = event['Records'][0]['s3']['object']['key']
+
+print(f"File '{object_key}' was uploaded to the bucket '{bucket_name}'")
+
+# Send notification via SNS
+sns_client = boto3.client('sns')
+topic_arn = 'arn:aws:sns:<region>:<account-id>:s3-lambda-sns'
+sns_client.publish(
+  Topic_ARN=topic_arn,
+  Subject='S3 Object created',
+  Message=f"File '{object_key}' was uploaded to the bucket '{bucket_name}'"
+)
+
+return {
+  'statusCode' : 200,
+  'body' : json.dumps('Lambda executed successfully')
+}
+
+  
+  
+  
